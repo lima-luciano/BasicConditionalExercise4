bcfbe356cf1a4a37059f2ca1af78d17a4004a76c
Devllu <dev.limaluciano@gmail.com>
2022-04-14 23:23:44 -0300

    Add project files.

---
+++ BasicConditionalExercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿// 20) Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou ÍMPAR.
+
+Console.WriteLine("Saiba se um numero é PAR ou IMPAR. \nDigite um numero:");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"O numero {number} é PAR");
+}
+else
+{
+    Console.WriteLine($"O numero {number} é IMPAR");
+}
